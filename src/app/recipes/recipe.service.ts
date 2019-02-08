a66fe159ebc3830ad44a129f105ede538fbf93aa
Herman Wu <herman@cloudistics.com>
2019-02-07 17:54:32 -0800

    add observable pattern

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,9 +4,7 @@ import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
 @Injectable()
-export class RecipeService {
-  recipeSelected = new EventEmitter<Recipe>();
-  
+export class RecipeService {  
   private recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
       new Ingredient('Meat', 1),
