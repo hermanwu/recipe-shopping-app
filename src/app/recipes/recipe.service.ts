8c317766b30afec3a9836f3cb3fa70abef7f708e
Herman Wu <herman@cloudistics.com>
2019-02-09 10:50:31 -0800

    implement forms

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,32 +1,54 @@
-import { EventEmitter, Injectable} from '@angular/core';
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
-import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { Subject } from "rxjs/Subject";
 
 @Injectable()
-export class RecipeService {  
-  private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
-      new Ingredient('Meat', 1),
-      new Ingredient('French Fries', 20)
-    ]),
-    new Recipe('A New Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg', [
-      new Ingredient('Chcken', 1),
-    ])
-  ]; 
+export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
-  constructor(private slService: ShoppingListService) {
-  }
+  private recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test",
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      [new Ingredient("Meat", 1), new Ingredient("French Fries", 20)]
+    ),
+    new Recipe(
+      "A New Test Recipe",
+      "This is simply a test",
+      "https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg",
+      [new Ingredient("Chcken", 1)]
+    )
+  ];
+
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
   }
 
-  addIngredientsToShoppingList(ingredients: Ingredient[] ) {
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.slService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
   }
 
   getRecipe(index: number) {
     return this.recipes[index];
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
