b2d343078858df77944f824be37ec6d4bdd89613
Herman <zeesun.wu@gmail.com>
2019-02-10 08:01:10 -0800

    add auth folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 
 const appRoutes: Routes = [
@@ -13,9 +14,10 @@ const appRoutes: Routes = [
     { path: '', component: RecipeStartComponent },
     { path: 'new', component: RecipeEditComponent },
     { path: ':id', component: RecipeDetailComponent },
-    { path: ':id/edit', component: RecipeEditComponent }, 
+    { path: ':id/edit', component: RecipeEditComponent },
   ]},
   { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
